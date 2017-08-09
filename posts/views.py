d7dde32d21ef914f9d6319d0dbebb91edeb89e35
alaazamani <alaa.zamani@gmail.com>
2017-08-09 17:49:38 +0300

    Images got Posts

--- posts/views.py
+++ posts/views.py
@@ -7,7 +7,7 @@ from urllib.parse import quote
 from .forms import PostForm
 
 def post_home(request):
-    return HttpResponse("<h1> Hello</h1>")
+	return HttpResponse("<h1> Hello</h1>")
 
 
 def post_detail(request, post_id):
@@ -20,29 +20,29 @@ def post_detail(request, post_id):
 
 
 def post_create(request):
-    form = PostForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        messages.success(request, "Successfully Created!")
-        return redirect("posts:list")
-    context = {
-    "title": "Create",
-    "form": form,
-    }
-    return render(request, 'post_create.html', context)
+	form = PostForm(request.POST or None, request.FILES or None)
+	if form.is_valid():
+		form.save()
+		messages.success(request, "Successfully Created!")
+		return redirect("posts:list")
+	context = {
+	"title": "Create",
+	"form": form,
+	}
+	return render(request, 'post_create.html', context)
 
 def post_list(request):
-    object_list = Post.objects.all() #.order_by("-timestamp","-updated")
+	object_list = Post.objects.all() #.order_by("-timestamp","-updated")
 
-    paginator = Paginator(object_list, 5) # Show 5 contacts per page
-    page = request.GET.get('page')
-    try:
-        objects = paginator.page(page)
-    except PageNotAnInteger:
-        objects = paginator.page(1)
-    except EmptyPage:
-        objects = paginator.page(paginator.num_pages)
-    context = {
+	paginator = Paginator(object_list, 5) # Show 5 contacts per page
+	page = request.GET.get('page')
+	try:
+		objects = paginator.page(page)
+	except PageNotAnInteger:
+		objects = paginator.page(1)
+	except EmptyPage:
+		objects = paginator.page(paginator.num_pages)
+	context = {
 	"object_list": objects,
 	"title": "List",
 	"user": request.user
@@ -50,21 +50,21 @@ def post_list(request):
 	return render(request, 'post_list.html', context)
 
 def post_update(request, post_id):
-    instance = get_object_or_404(Post, id=post_id)
-    form = PostForm(request.POST or None, instance = instance)
-    if form.is_valid():
-        form.save()
-        messages.success(request, "Successfully Edited!")
-        return redirect(instance.get_absolute_url())
-    context = {
-    "form":form,
-    "instance": instance,
-    "title": "Update",
-    }
-    return render(request, 'post_update.html', context)
+	instance = get_object_or_404(Post, id=post_id)
+	form = PostForm(request.POST or None, instance = instance, request.FILES or None)
+	if form.is_valid():
+		form.save()
+		messages.success(request, "Successfully Edited!")
+		return redirect(instance.get_absolute_url())
+	context = {
+	"form":form,
+	"instance": instance,
+	"title": "Update",
+	}
+	return render(request, 'post_update.html', context)
 
 def post_delete(request, post_id):
-    instance = get_object_or_404(Post, id=post_id)
-    instance.delete()
-    messages.success(request, "Successfully Deleted!")
-    return redirect("posts:list")
+	instance = get_object_or_404(Post, id=post_id)
+	instance.delete()
+	messages.success(request, "Successfully Deleted!")
+	return redirect("posts:list")
